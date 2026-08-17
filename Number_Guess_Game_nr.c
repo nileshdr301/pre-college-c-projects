3b1f25f8cb21d428ee2fb1a404d6d4c46e893475
Nilesh Raut <nileshdr301@gmail.com>
2026-08-17 09:45:01 +0530

    Add files via upload
    # Number Guessing Game 🎯
    
    A simple **Number Guessing Game built using C programming**. The player has to guess the correct number, and the game continues until the correct number **50** is entered.
    
    ### Features
    
    * 🎮 Simple and beginner-friendly gameplay
    * 🔢 User input for guessing the number
    * ✅ Displays a success message for the correct guess
    * ❌ Displays "Wrong Guess" and allows the player to try again
    * 🔄 Uses a `do-while` loop to repeat the game
    
    ### Concepts Used
    
    * `printf()` and `scanf()`
    * Variables
    * `if-else` statements
    * `do-while` loop
    * User input and output
    
    **Language:** C
    **Created by:** Nilesh Raut

---
+++ Number_Guess_Game_nr.c
@@ -0,0 +1,29 @@
+//number gusing game
+
+#include<stdio.h>
+int main()
+{
+	
+	int number = 50;
+	printf("Let's Start the Number Guuessing Game\n");
+	printf("Created by Nilesh Raut\n");
+	
+
+	do{
+			printf("================\n");
+	printf("Enter Guess A number\n");
+	printf("================\n");
+	
+	scanf("%d", &number);
+	
+	if(number==50){
+		printf("Correct Guess \n Congratulations you guess correct number.\n Keep it up\n");
+		printf("Thank's for playing this game");
+	}
+	else{printf("Wrong Guess \n Try Again\n");
+	}
+}while(number!=50);
+
+	
+	return 0;
+}
